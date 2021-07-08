3509e5c225947aabc4c3bf4d4228f3efb4953024
Nathaniel <nathanwriting@126.com>
2021-07-08 19:20:15 +0800

    更新逻辑

--- main.py
+++ main.py
@@ -3,20 +3,22 @@
 from datetime import datetime
 
 import data_utils
+from qihu import qihu
+from yyb import yyb
 
 if __name__ == '__main__':
     name = data_utils.parse_cfg('download', 'type', 'yyb')
     start_time = datetime.today().now()
-    data_utils.merge_data()
-    # if name == 'qihu':
-    #     # 360 应用市场
-    #     a = qihu()
-    #     a.start()
-    # elif name == 'yyb':
-    #     # 应用宝
-    #     b = yyb()
-    #     b.start()
-    # else:
-    #     print('unsupported market : %s ' % name)
+    # data_utils.merge_data()
+    if name == 'qihu':
+        # 360 应用市场
+        a = qihu()
+        a.start()
+    elif name == 'yyb':
+        # 应用宝
+        b = yyb()
+        b.start()
+    else:
+        print('unsupported market : %s ' % name)
     duration = datetime.today().now() - start_time
     print("duration : %s" % duration)
