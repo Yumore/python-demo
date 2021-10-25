29d6857eae9287864ca2a511f1b2c9275787919f
Nathaniel <nathanwriting@126.com>
2021-10-25 19:14:59 +0800

    更新代码逻辑

--- virus_scan.py
+++ virus_scan.py
@@ -19,14 +19,13 @@ class virus_scan:
         for index, table in enumerate(tables, start=0):
             if index == 1:
                 print("分页的table: %s" % table)
+                continue
             else:
-                print("*" * 50)
-                # print(table)
                 # 匹配<th></th>之间的内容
                 # 因为<tr>标签大多数不是在同一行，所以要加 re.S和re.M多行匹配
                 trs = re.findall(r"<tr.*?>(.*?)</tr>", table, re.S | re.M)
+                data_list = []
                 for indicate, tr in enumerate(trs, start=0):
-                    print("tr: %s" % tr)
                     # tr = tr.replace('&nbsp;', '').replace('/> ', '/>').replace('> <', '><')
                     # 匹配<td></td>之间的内容
                     # tds = re.findall(r"<td>(.*?)</td>", tr, re.S | re.M)
@@ -35,16 +34,21 @@ class virus_scan:
                         sql_data = []
                         for position, td in enumerate(tds, start=0):
                             print('position=>td : %s=>%s' % (position, td))
-                            values = re.findall(r'<a.*?>(.*?)</a>', td, re.S | re.M)
+                            if position == 1 and "Found" in tr:
+                                print("X" * 30, "没有找到病毒", "X" * 30)
+                                continue
+                            values = re.findall(r'<a .*?>(.*?)</a>', td, re.S | re.M)
                             hrefs = re.findall(r"(?<=href=\").+?(?=\")|(?<=href=\').+?(?=\')", td, re.I | re.S | re.M)
                             print("values: %s --- hrefs: %s" % (values, hrefs))
                             sql_data.extend(values)
                             sql_data.extend(hrefs)
                         print("sql_data: %s" % sql_data)
-                        # data_utils.update_bazaar(sql_data)
+                        data_list.append(sql_data)
                     else:
                         ths = re.findall(r"<th[^>]*>(.*?)</th>", tr, re.S | re.M)
                         print("ths: %s" % ths)
+                        continue
+                print("data_list: %s" % data_list)
 
     def start(self):
         # data_utils.csv_mysql("./full-back.csv")
